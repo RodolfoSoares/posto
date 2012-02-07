a81911131c5e730fc084e950e2de99ce6b7a4db8
RodolfoSoares <lab08@ama-desktop.(none)>
2012-02-07 10:24:02 -0200

    criação do projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Posto::Application.config.secret_token = '157d5d89ccfc44c957cbd9f25970ba8247b9b613206c4896074469c15166c86ac24c59a1f099c0b3cdd7eed2cd3d4493f6d7b0439e0825e97d3dc7106f3dec45'
