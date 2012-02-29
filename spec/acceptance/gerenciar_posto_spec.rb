00f9df95ce07f47510a71eb0e7741545482230cd
RodolfoSoares <lab08@ama-desktop.(none)>
2012-02-29 10:12:01 -0300

    Alterar LocalPosto

--- spec/acceptance/gerenciar_posto_spec.rb
+++ spec/acceptance/gerenciar_posto_spec.rb
@@ -44,5 +44,41 @@ feature 'gerenciar postocombustivel' do
 
   end
 
+    scenario 'alterar postocombustivel ' do #, :javascript => true do
+
+    marca = FactoryGirl.create(:marca,:nome => 'Ipyranga')
+    postocombustivel = FactoryGirl.create(:postocombustivel, :marca => marca)
+
+    visit edit_postocombustivel_path(postocombustivel)
+
+    fill_in 'Nome', :with => 'Arara Azul'
+    fill_in 'postocombustivel_endereco', :with => 'Av. 28 de março'
+    fill_in 'Telefone' , :with => '123456789'
+    fill_in 'Cep', :with => '28020152' 
+    fill_in 'Cnpj', :with => '65443514264'
+    fill_in 'Email', :with => 'araraazul@gmail.com'   
+    fill_in 'postocombustivel_mediacombustivel', :with => '3'
+    fill_in 'postocombustivel_mediaatendimento', :with => '4'
+    fill_in 'postocombustivel_mediapreco', :with => '4'
+    fill_in 'postocombustivel_mediageral', :with => '3'
+    
+    select('Ipyranga', from: "Marca")
+
+       
+    click_button 'Salvar'
+  
+  
+    page.should have_content 'Nome: Arara Azul'
+    page.should have_content 'Endereço: Av. 28 de março'
+    page.should have_content 'Telefone: 123456789'
+    page.should have_content 'Cep: 28020152'
+    page.should have_content 'Cnpj: 65443514264'
+    page.should have_content 'Email: araraazul@gmail.com'
+    page.should have_content 'Mediacombustivel: 3'
+    page.should have_content 'Mediaatendimento: 4'
+    page.should have_content 'Mediapreco: 4'
+    page.should have_content 'Mediageral: 3'
+    page.should have_content 'Marca: Ipyranga'
+  end
  
 end
