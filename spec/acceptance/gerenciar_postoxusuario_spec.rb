72cb1f5fb3c7ad6507fc0ef019ef68c391cf2474
RodolfoSoares <lab08@ama-desktop.(none)>
2012-02-29 12:26:12 -0300

    Alterar Postousuario

--- spec/acceptance/gerenciar_postoxusuario_spec.rb
+++ spec/acceptance/gerenciar_postoxusuario_spec.rb
@@ -28,5 +28,32 @@ feature 'gerenciar postoxusuario' do
     page.should have_content 'Notageral: 3'
     page.should have_content 'Usuario: Juca'
     page.should have_content 'Postocombustivel: Arara'
+
+    end
+
+    scenario 'alterar postocombustivel ' do #, :javascript => true do
+
+    usuario = FactoryGirl.create(:usuario,:login => 'Juca') 
+    postocombustivel = FactoryGirl.create(:postocombustivel,:nome => 'Arara')  
+    postousuario = FactoryGirl.create(:postousuario, :usuario => usuario , :postocombustivel => postocombustivel) 
+
+
+    visit edit_postousuario_path(postousuario)
+
+
+    fill_in 'Notaatendimento', :with => '4'
+    fill_in 'Notageral', :with => '3'
+    
+    select('Juca', from: "Usuario")
+    select('Arara', from: "Postocombustivel")
+
+       
+    click_button 'Salvar'
+  
+  
+    page.should have_content 'Notaatendimento: 4'
+    page.should have_content 'Notageral: 3'
+    page.should have_content 'Usuario: Juca'
+    page.should have_content 'Postocombustivel: Arara'
     end
   end
